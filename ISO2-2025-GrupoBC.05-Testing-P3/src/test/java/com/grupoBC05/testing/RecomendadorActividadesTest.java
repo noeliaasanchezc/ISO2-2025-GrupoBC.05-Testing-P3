762a7179a913a99bdff3035174aa957203e82b6d
noeliaasanchezc <noelia.palomino1@alu.uclm.es>
2025-12-10 18:21:10 +0100

    informes

--- ISO2-2025-GrupoBC.05-Testing-P3/src/test/java/com/grupoBC05/testing/RecomendadorActividadesTest.java
+++ ISO2-2025-GrupoBC.05-Testing-P3/src/test/java/com/grupoBC05/testing/RecomendadorActividadesTest.java
@@ -3,67 +3,61 @@ package com.grupoBC05.testing;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RecomendadorActividadesTest {
+public class RecomendadorActividadesTest {
 
     RecomendadorActividades r = new RecomendadorActividades();
 
     @Test
-    void testNoPlenasFacultades() {
-        String res = r.recomendarActividad(false, false, 20, 40, "ninguna", false, false);
+    void testSinFacultades() {
+        String res = r.recomendarActividad(false, false, 20, 50, "ninguna", false, false);
         assertEquals("Ninguna actividad", res);
     }
 
     @Test
-    void testConSintomas() {
-        String res = r.recomendarActividad(true, true, 20, 40, "ninguna", false, false);
+    void testSintomasRecientes() {
+        String res = r.recomendarActividad(true, true, 20, 50, "ninguna", false, false);
         assertEquals("Ninguna actividad", res);
     }
 
     @Test
-    void testNevadaQuedarseEnCasa() {
-        String res = r.recomendarActividad(true, false, -1, 10, "nieve", false, false);
+    void testFrioNieveAgua() {
+        String res = r.recomendarActividad(true, false, -2, 10, "nieve", false, false);
         assertEquals("Quedarse en casa", res);
     }
 
     @Test
-    void testLluviaQuedarseEnCasa() {
-        String res = r.recomendarActividad(true, false, -1, 10, "agua", false, false);
-        assertEquals("Quedarse en casa", res);
-    }
-
-    @Test
-    void testEsquiarDisponible() {
+    void testFrioSecoEsquiarDisponible() {
         String res = r.recomendarActividad(true, false, -1, 10, "ninguna", false, false);
         assertEquals("Esquiar", res);
     }
 
     @Test
-    void testEsquiarNoDisponibleAforo() {
+    void testFrioSecoEsquiarNoAforo() {
         String res = r.recomendarActividad(true, false, -1, 10, "ninguna", true, false);
         assertEquals("Actividad no disponible por aforo", res);
     }
 
     @Test
     void testSenderismoDisponible() {
-        String res = r.recomendarActividad(true, false, 10, 40, "ninguna", false, false);
+        String res = r.recomendarActividad(true, false, 10, 30, "ninguna", false, false);
         assertEquals("Senderismo o escalada", res);
     }
 
     @Test
-    void testSenderismoNoDisponibleAforo() {
-        String res = r.recomendarActividad(true, false, 10, 40, "ninguna", true, false);
+    void testSenderismoNoAforo() {
+        String res = r.recomendarActividad(true, false, 10, 30, "ninguna", true, false);
         assertEquals("Actividad no disponible por aforo", res);
     }
 
     @Test
-    void testActividadesVerano() {
-        String res = r.recomendarActividad(true, false, 20, 50, "ninguna", false, false);
+    void testActividadesPrimaveraDisponible() {
+        String res = r.recomendarActividad(true, false, 20, 40, "ninguna", false, false);
         assertEquals("Actividades de primavera/verano/otoño", res);
     }
 
     @Test
-    void testActividadesVeranoNoAforo() {
-        String res = r.recomendarActividad(true, false, 20, 50, "ninguna", true, false);
+    void testActividadesPrimaveraNoAforo() {
+        String res = r.recomendarActividad(true, false, 20, 40, "ninguna", true, false);
         assertEquals("Actividad no disponible por aforo", res);
     }
 
@@ -75,19 +69,20 @@ class RecomendadorActividadesTest {
 
     @Test
     void testPlayaPiscinaDisponible() {
-        String res = r.recomendarActividad(true, false, 32, 40, "ninguna", false, false);
+        // Solo ocurre con temperatura > 35
+        String res = r.recomendarActividad(true, false, 36, 40, "ninguna", false, false);
         assertEquals("Playa o piscina", res);
     }
 
     @Test
     void testPlayaPiscinaNoDisponibleAforo() {
-        String res = r.recomendarActividad(true, false, 32, 40, "ninguna", true, false);
+        String res = r.recomendarActividad(true, false, 36, 40, "ninguna", true, false);
         assertEquals("Actividad no disponible por aforo", res);
     }
 
     @Test
-    void testSinRecomendacionGeneral() {
-        String res = r.recomendarActividad(true, false, 15, 90, "lluvia", false, true);
+    void testSinRecomendacion() {
+        String res = r.recomendarActividad(true, false, 10, 40, "lluvia", false, true);
         assertEquals("Sin recomendación", res);
     }
 }
